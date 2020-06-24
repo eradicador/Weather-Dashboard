495dd1001418440547bca4dbdfb3a44d3b160b1a
eradicador <janisepinto@yahoo.com>
2020-06-23 21:12:55 -0400

    added click function for search button

--- assets/js/scrpit.js
+++ assets/js/scrpit.js
@@ -1,4 +1,3 @@
-
 var queryURL = http:"api.openweathermap.org/data/2.5/forecast?id=524901&APPID={90a083dba21f51fa89fd107b2a602ddf}"
 
 // Begin creating basic click events. Register the submit button
@@ -8,3 +7,24 @@ $.ajax({
     method: "GET"
 }).then(updatePage);
 console.log(queryURL);
+
+// .on("click") function associated with the Search Button
+$("#run-search").on("click", function(event) {
+    // This line allows us to take advantage of the HTML "submit" property
+    // This way we can hit enter on the keyboard and it registers the search
+    // (in addition to clicks). Prevents the page from reloading on form submit.
+    event.preventDefault();
+
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
