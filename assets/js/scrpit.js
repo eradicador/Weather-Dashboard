d9dac7d65a5c74c031ad1e121903fd7d84af5f7a
eradicador <janisepinto@yahoo.com>
2020-06-23 20:53:44 -0400

    added AJAX request to API

--- assets/js/scrpit.js
+++ assets/js/scrpit.js
@@ -1,2 +1,10 @@
-API call:
-http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={90a083dba21f51fa89fd107b2a602ddf}
+
+var queryURL = http:"api.openweathermap.org/data/2.5/forecast?id=524901&APPID={90a083dba21f51fa89fd107b2a602ddf}"
+
+// Begin creating basic click events. Register the submit button
+//Make the AJAX request to the API - GETs the JSON data at the queryURL
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(updatePage);
+console.log(queryURL);
