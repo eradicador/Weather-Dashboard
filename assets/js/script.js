d11196c67a17bac094d1efa9c4b2f5a28769b254
eradicador <janisepinto@yahoo.com>
2020-06-25 00:50:45 -0400

    created images folder, added screenshot of application

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
 // serach parameters
 // var queryURL = "http:api.openweathermap.org/data/2.5/forecast?id=524901&APPID=c5ddeb8a63142d853cedb5adfd31187a"
 var APIKey = 'c5ddeb8a63142d853cedb5adfd31187a';
-var cityInfo = ""
-// var temp = document.querySelector("#temperature")
-var humid = ""
-var wind = ""
-var uv = ""
-var cities = []
+var cityInfo = "";
+var button = document.getElementById("run-search");
 
 // .on("click") function associated with the Search Button
 $("#run-search").on("click", function (event) {
@@ -24,7 +20,7 @@ $("#run-search").on("click", function (event) {
         updatePage(data);
     });
 
-    
+
     // This way we can hit enter on the keyboard and it registers the search
     // (in addition to clicks). Prevents the page from reloading on form submit.
     event.preventDefault();
@@ -32,7 +28,7 @@ $("#run-search").on("click", function (event) {
 //Get from the form the number of results to display
 function updatePage(cityInfo) {
     console.log(cityInfo);
-    $('#date').text(moment().format('M/Do/YYYY'));
+    $('#date').text(moment().format('M/D/YYYY'));
     // Key.value
     $("#city-name").text(cityInfo.name);
     // console.log(cityInfo);
@@ -47,13 +43,20 @@ function updatePage(cityInfo) {
 }
 // weather data, 3 api calls each function its own ajax call and jquery ,weather data for city, 5 day forecast, last api call the uv index append text add event listener elements to the page.
 $('.search-input').val(localStorage.getItem('#city-search'))
-function storeCities(){
+function storeCities() {
     var storageStuff = localStorage.setItem('currentCity', searchInfo);
     clearInterval()
     // localStorage.setItem("currentCity", searchInfo); 
-    console.log(localStorage); 
+    console.log(localStorage);
 }
 localStorage.getItem('currentCity')
+
+button.addEventListener("click", function () {
+    howYouFeel.textContent = button.p - 3;
+    localStorage.setItem('currentCity', button.p - 3);
+    console.log(localStorage);
+    howYouFeelSaved.textContent = localStorage.getItem('currentCity');
+})
 
 
 // if statements or loop, clear 
@@ -72,14 +75,3 @@ localStorage.getItem('currentCity')
 // }
 
 
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
